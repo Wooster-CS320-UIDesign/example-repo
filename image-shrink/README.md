56b16da5290fd9b1dde45bb01b695641c882d6e7
Drew Guarnera <dguarnera@wooster.edu>
2020-10-13 10:10:33 -0400

    Commit starter code

---
+++ image-shrink/README.md
@@ -0,0 +1,18 @@
+# ImageShrink Applicaiton
+
+## Usage
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To run electron
+
+```sh
+$ npm start
+```
---
+++ image-shrink/main.js
@@ -0,0 +1,145 @@
+const path = require('path');
+const os = require('os');
+
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
+const log = require('electron-log');
+
+// Set Environment
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+
+const isMac = process.platform === 'darwin' ? true : false 
+
+let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: 'Image Shrink',
+        width: isDev ? 800 : 500,
+        height: 600,
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: isDev,
+        backgroundColor: 'white',
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+
+    });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.loadFile('./app/index.html');
+    // EQUIVALENT
+    //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+}
+
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About Image Shrink',
+        width: 300,
+        height: 300,
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: false,
+        backgroundColor: 'white'
+    });
+
+    aboutWindow.loadFile('./app/about.html');
+    // EQUIVALENT
+    //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+}
+
+ipcMain.on('image:minimize', (e, options) => {
+    options.dest = path.join(os.homedir(), 'imageshrink')
+    shrinkImage(options);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+    try {
+        const pngQuality = quality / 100;
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest, 
+            plugins: [
+                imageminMozjpeg({quality}),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        })
+
+        log.info(files);
+        shell.openPath(dest);
+
+        mainWindow.webContents.send('image:done');
+    }
+    catch (err) {
+        log.error(err);
+    }
+}
+
+app.on('ready', () => {
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('ready', () => mainWindow = null);
+});
+
+const menu = [
+    ...(isMac ? [{ 
+        label: app.name,
+        submenu: [
+            {
+                label: 'About',
+                click: createAboutWindow
+            },
+            { role: 'quit' }
+        ]
+    }] : []),
+    { 
+        role: 'fileMenu'
+    },
+    ...(!isMac ? [
+        {
+            label: 'About',
+            click: createAboutWindow
+        }
+    ] : []),
+    ...(isDev ? [
+        { 
+            label: 'Developer',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { type: 'separator' },
+                { role: 'toggledevtools' }
+            ]
+        }
+    ]: [])
+];
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow()
+    }
+  })
